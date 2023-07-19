5507554aa244a00692b20b8fffa86b72df13cdca
Florian Kotthoff <florian.kotthoff@posteo.de>
2023-07-19 15:42:47 +0200

    Initial translation of sidebar header #39

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -1,39 +1,42 @@
-import React, { useRef } from "react";
+import React from "react";
+import { useTranslation } from "react-i18next";
 import ContactIcons from "../Contact/ContactIcons";
 import DatesSlider from "../PVSimulation/DatesSlider";
-import RadiusSlider from "../PVSimulation/RadiusSlider";
 import DistanceSlider from "../PVSimulation/DistanceSlider";
-import DraggableCircle from "../PVSimulation/DirectionSlider";
+import RadiusSlider from "../PVSimulation/RadiusSlider";
 
-const SideBar = () => (
-  <section id="sidebar">
-    <section id="intro">
-      <header>
-        <h2>Anleitung</h2>
-      </header>
-    </section>
-    <section className="blurb">
-      <p>
-        Im Suchfeld einfach deine Adresse oder deine Koordinaten eingeben. Wir
-        berechnen dann das Potential für eine Solaranlage auf deiner Dach- und
-        Fassadenfläche.
-      </p>
-      <p>Der Service funktioniert aktuell nur für Gebäude in Bayern.</p>
-      <DatesSlider></DatesSlider>
-      <RadiusSlider></RadiusSlider>
-      <DistanceSlider></DistanceSlider>
-      {/* <DraggableCircle></DraggableCircle> */}
-      <p id="status">Warte auf Adresseingabe</p>
-    </section>
+const SideBar = () => {
+  const { t, i18n } = useTranslation();
+  return (
+    <section id="sidebar">
+      <section id="intro">
+        <header>
+          <h2>{t("sidebar.header")}</h2>
+        </header>
+      </section>
+      <section className="blurb">
+        <p>
+          Im Suchfeld einfach deine Adresse oder deine Koordinaten eingeben. Wir
+          berechnen dann das Potential für eine Solaranlage auf deiner Dach- und
+          Fassadenfläche.
+        </p>
+        <p>Der Service funktioniert aktuell nur für Gebäude in Bayern.</p>
+        <DatesSlider></DatesSlider>
+        <RadiusSlider></RadiusSlider>
+        <DistanceSlider></DistanceSlider>
+        {/* <DraggableCircle></DraggableCircle> */}
+        <p id="status">Warte auf Adresseingabe</p>
+      </section>
 
-    <section id="footer">
-      <ContactIcons />
-      <p className="copyright">
-        &copy; Erstellt von Flo, Martin und Korbi,{" "}
-        <a href="/Impressum">Impressum</a>.
-      </p>
+      <section id="footer">
+        <ContactIcons />
+        <p className="copyright">
+          &copy; Erstellt von Flo, Martin und Korbi,{" "}
+          <a href="/Impressum">Impressum</a>.
+        </p>
+      </section>
     </section>
-  </section>
-);
+  );
+};
 
 export default SideBar;
