b975fb8f2803e5f9703997185fcbb4afe3563bc2
Automated <actions@users.noreply.github.com>
2023-08-07 10:51:57 +0000

    Updates

--- static/js/922.afc700da.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[922],{8922:function(n,e,t){t.r(e);t(2791);var o=t(1087),s=t(6907),c=t(184);e.default=function(){return(0,c.jsx)(s.B6,{children:(0,c.jsxs)("div",{className:"not-found",children:[(0,c.jsx)(s.ql,{title:"404 Not Found",children:(0,c.jsx)("meta",{name:"description",content:"The content you are looking for cannot be found."})}),(0,c.jsx)("h1",{children:"Page Not Found"}),(0,c.jsxs)("p",{children:["Return ",(0,c.jsx)(o.rU,{to:"/",children:"home"}),"."]})]})})}}}]);
-//# sourceMappingURL=922.afc700da.chunk.js.map
