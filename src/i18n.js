58a2bb9b80722b6c062f50f41689c4fad0452834
Florian Kotthoff <74312290+FlorianK13@users.noreply.github.com>
2024-06-25 10:49:05 +0200

    Change default language to german

--- src/i18n.js
+++ src/i18n.js
@@ -17,7 +17,7 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: "en",
+    fallbackLng: "de",
     debug: true,
   })
 
