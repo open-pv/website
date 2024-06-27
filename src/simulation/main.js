96a374ed2a0d7d38f7b2e67fd3b89f28d9110ef0
Konrad Heidler <k.heidler@tum.de>
2024-06-27 22:54:26 +0200

    Don't forget to add simulationGeometry as a shadingGeometry! :)

--- src/simulation/main.js
+++ src/simulation/main.js
@@ -1,6 +1,7 @@
 // This is the only file where both functionality and GUI stuff is allowed
 
 import ShadingScene from "@openpv/simshady";
+import * as THREE from "three";
 import { downloadBuildings } from "./download";
 import { setLocation } from "./location";
 import { processGeometries } from "./preprocessing";
@@ -18,13 +19,24 @@ export async function main(inputValue, inputChanged, oldLocation) {
       console.log(k, geometries[k]);
     }
     const scene = new ShadingScene(location.lat, location.lon);
-    geometries.simulation.forEach(geom => scene.addSimulationGeometry(geom));
-    geometries.surrounding.forEach(geom => scene.addShadingGeometry(geom));
+    geometries.simulation.forEach(geom => {
+      scene.addSimulationGeometry(geom);
+      scene.addShadingGeometry(geom);
+    });
+    geometries.surrounding.forEach(geom => {
+      scene.addShadingGeometry(geom);
+    });
 
     initializeViewer(geometries, inputChanged);
     scene.calculate(window.numSimulations).then(simulationMesh => {
       window.setLoading(false);
       console.log(simulationMesh);
+      const material = new THREE.MeshLambertMaterial({
+        vertexColors: true,
+        side: THREE.DoubleSide,
+      });
+      simulationMesh.material = material;
+
       swapSimulationMesh(simulationMesh);
     });
   } else {
