35d9fb4c449984f0bf5db81315c132959fe212bb
Florian Kotthoff <74312290+FlorianK13@users.noreply.github.com>
2024-07-08 09:20:16 +0200

    Add buttons on top of map

--- src/components/ThreeViewer/ThreeViewer.js
+++ src/components/ThreeViewer/ThreeViewer.js
@@ -24,27 +24,47 @@ export default function ThreeViewer() {
     <div className="viewer-container" style={{ position: "relative" }}>
       <Canvas className="three-viewer" flat linear></Canvas>
       <canvas id="canvas" width={0} height={0}></canvas>
+
       {window.showViridisLegend && (
-        <div
-          style={{ position: "absolute", left: 0, bottom: 0, margin: "10px" }}
-        >
-          <p style={{ fontSize: "8px" }}>
-            <a href="https://geodaten.bayern.de/opengeodata/">Gebäudedaten</a>{" "}
-            der
-            <a href="https://www.ldbv.bayern.de/vermessung/bvv.html">
-              {" "}
-              Bayerischen Vermessungsverwaltung{" "}
-            </a>{" "}
-            |
-            <a href="https://creativecommons.org/licenses/by/4.0/deed.de">
-              CC BY 4.0
-            </a>
-          </p>
-          <p>
-            <b>Leertaste</b>: Eckpunkt <b>P</b>: Polygon erstellen <b>R</b>:
-            Alles zurücksetzen
-          </p>
-        </div>
+        <>
+          <div
+            style={{
+              display: "grid",
+              gridTemplateColumns: "1fr 1fr 1fr",
+              gap: "10px",
+              position: "absolute",
+              top: 0,
+              margin: "10px",
+            }}
+          >
+            <button style={{ width: "100%", height: "50px" }}>
+              PV Anlage einzeichnen
+            </button>
+            <button style={{ width: "100%", height: "50px" }}>
+              Parameter ändern
+            </button>
+          </div>
+          <div
+            style={{ position: "absolute", left: 0, bottom: 0, margin: "10px" }}
+          >
+            <div id="footer">
+              <p class="copyright">
+                <a href="https://geodaten.bayern.de/opengeodata/">
+                  Gebäudedaten
+                </a>{" "}
+                der
+                <a href="https://www.ldbv.bayern.de/vermessung/bvv.html">
+                  {" "}
+                  Bayerischen Vermessungsverwaltung{" "}
+                </a>{" "}
+                |
+                <a href="https://creativecommons.org/licenses/by/4.0/deed.de">
+                  CC BY 4.0
+                </a>
+              </p>
+            </div>
+          </div>
+        </>
       )}
     </div>
   )
