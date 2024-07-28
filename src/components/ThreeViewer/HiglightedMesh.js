0cb65abae3bbe427247fb85e46b0c2bfe1fc34a3
Florian Kotthoff <74312290+FlorianK13@users.noreply.github.com>
2024-07-28 16:35:06 +0200

    Make highlighted mesh dark

--- src/components/ThreeViewer/HiglightedMesh.js
+++ src/components/ThreeViewer/HiglightedMesh.js
@@ -1,9 +1,18 @@
 import { Edges } from "@react-three/drei"
 import React from "react"
+import * as THREE from "three"
 
 export function HighlightedMesh({ geometry, material }) {
   return (
-    <mesh geometry={geometry} material={material}>
+    <mesh
+      geometry={geometry}
+      material={
+        new THREE.MeshStandardMaterial({
+          color: "#2b2c40",
+          transparent: false,
+        })
+      }
+    >
       <Edges color="orange" lineWidth={5} />
     </mesh>
   )
