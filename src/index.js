9b7bbc4f1634a0a4c5c14274d5dfbe1042ae20d6
Florian Kotthoff <74312290+FlorianK13@users.noreply.github.com>
2024-07-28 19:33:01 +0200

    Refactor - Delete App.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
-import React from "react"
+import React, { Suspense, lazy } from "react"
 import { createRoot, hydrateRoot } from "react-dom/client"
-import App from "./App"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import "./i18n" // needs to be bundled
+import Main from "./layouts/Main" // fallback for lazy pages
+import "./static/css/main.scss" // All of our styles
+
+const { PUBLIC_URL } = process.env
+
+// Every route - we lazy load so that each page can be chunked
+// NOTE that some of these chunks are very small. We should optimize
+// which pages are lazy loaded in the future.
+const Index = lazy(() => import("./pages/Index"))
+const NotFound = lazy(() => import("./pages/NotFound"))
+const Impressum = lazy(() => import("./pages/Impressum"))
+const Datenschutz = lazy(() => import("./pages/Datenschutz"))
+const About = lazy(() => import("./pages/About"))
 
 // See https://reactjs.org/docs/strict-mode.html
 const StrictApp = () => (
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={PUBLIC_URL}>
+      <Suspense fallback={<Main />}>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/anleitung" element={<About />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/impressum" element={<Impressum />} />
+          <Route path="/datenschutz" element={<Datenschutz />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   </React.StrictMode>
 )
 
