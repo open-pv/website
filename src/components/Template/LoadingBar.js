e004ec4e2833543155beff837af83720ecce5cf4
Florian Kotthoff <74312290+FlorianK13@users.noreply.github.com>
2024-07-28 19:52:45 +0200

    Add usage hints to loading bar

--- src/components/Template/LoadingBar.js
+++ src/components/Template/LoadingBar.js
@@ -1,12 +1,18 @@
 import PropTypes from "prop-types"
 import React from "react"
+import { useTranslation } from "react-i18next"
 
 const LoadingBar = ({ progress }) => {
+  const { t, i18n } = useTranslation()
+  const numberTips = 3
+  const shownTip = Math.floor(Math.random() * numberTips) + 1
+
   const containerStyle = {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     height: "100vh",
+    flexDirection: "column",
   }
 
   const barContainerStyle = {
@@ -24,6 +30,7 @@ const LoadingBar = ({ progress }) => {
 
   return (
     <div style={containerStyle}>
+      <p>{t("loadingMessage.tip" + shownTip.toString())}</p>
       <div style={barContainerStyle}>
         <div style={barStyle}></div>
       </div>
