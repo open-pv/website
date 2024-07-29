d2f91008e9fc0afeb65b34abf397adf73e242168
Florian Kotthoff <74312290+FlorianK13@users.noreply.github.com>
2024-07-29 18:15:04 +0200

    Add comments to explain react states

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -10,15 +10,22 @@ import Scene from "../components/ThreeViewer/Scene"
 import Main from "../Main"
 
 function Index() {
-  // Frontend States
+  // frontendState defines the general state of the frontend (Map, Results, Loading, DrawPV)
   const [frontendState, setFrontendState] = useState("Map")
+  // showTerrain decides if the underlying Map is visible or not
   const [showTerrain, setShowTerrain] = useState(true)
+  // simulationProgress is used for the loading bar
   const [simulationProgress, setSimulationProgress] = useState(0)
+  // A list of visible PV Systems - they get visible after they are drawn on a building and calculated
   const [visiblePVSystems, setvisiblePVSystems] = useState([])
+  // When a building is selected from the Background/Surrounding to be simulated, it needs to be deleted
+  // from the background/surrounding mesh. These two states collect a list of names that should not be rendered
+  // Elements are for example "SurroundingMesh-37"
   const [deletedSurroundingMeshes, setDeletedSurroundingMeshes] = useState([])
   const [deletedBackgroundMeshes, setDeletedBackgroundMeshes] = useState([])
   window.setDeletedSurroundingMeshes = setDeletedSurroundingMeshes
   window.setDeletedBackgroundMeshes = setDeletedBackgroundMeshes
+  // The federal State where the material comes from, ie "BY"
   const [federalState, setFederalState] = useState(false)
   window.setFederalState = setFederalState
 
@@ -29,6 +36,7 @@ function Index() {
   })
   const [displayedSimulationMesh, setDisplayedSimulationMesh] = useState([])
   const [selectedMesh, setSelectedMesh] = useState([])
+  //geoLocation is an object with lat, lon
   const [geoLocation, setGeoLocation] = useState()
   window.setGeoLocation = setGeoLocation
 
