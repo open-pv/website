1d667866440ee12f500dc74b258bc675b88f59a1
Florian Kotthoff <74312290+FlorianK13@users.noreply.github.com>
2024-08-05 10:56:42 +0200

    Make multipage welcome message

--- src/components/Template/WelcomeMessage.js
+++ src/components/Template/WelcomeMessage.js
@@ -1,5 +1,8 @@
 import {
+  Box,
   Button,
+  Circle,
+  Flex,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -9,24 +12,65 @@ import {
   ModalOverlay,
   useDisclosure,
 } from "@chakra-ui/react"
-import React from "react"
+import React, { useState } from "react"
+import { useTranslation } from "react-i18next"
 
 function WelcomeMessage() {
   const { isOpen, onOpen, onClose } = useDisclosure({ defaultIsOpen: true })
+  const [currentPage, setCurrentPage] = useState(1)
+  const { t, i18n } = useTranslation()
+
+  const nextPage = () => {
+    if (currentPage < 3) setCurrentPage(currentPage + 1)
+  }
+
+  const prevPage = () => {
+    if (currentPage > 1) setCurrentPage(currentPage - 1)
+  }
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Modal Title</ModalHeader>
+        <ModalHeader>{t("WelcomeMessage.title")}</ModalHeader>
         <ModalCloseButton />
-        <ModalBody>Hello World</ModalBody>
+        <ModalBody>
+          {currentPage === 1 && <Box>{t("WelcomeMessage.firstPage")}</Box>}
+          {currentPage === 2 && <Box>Second Page Content</Box>}
+          {currentPage === 3 && <Box>Third Page Content</Box>}
+        </ModalBody>
 
         <ModalFooter>
-          <Button colorScheme="blue" mr={3} onClick={onClose}>
+          <Button
+            colorScheme="blue"
+            mr={3}
+            onClick={prevPage}
+            isDisabled={currentPage === 1}
+          >
+            {t("previous")}
+          </Button>
+          <Button
+            colorScheme="blue"
+            mr={3}
+            onClick={nextPage}
+            isDisabled={currentPage === 3}
+          >
+            {t("next")}
+          </Button>
+          <Button variant="ghost" onClick={onClose}>
             Close
           </Button>
-          <Button variant="ghost">Secondary Action</Button>
         </ModalFooter>
+        <Flex justifyContent="center" mb={4}>
+          {[1, 2, 3].map((page) => (
+            <Circle
+              key={page}
+              size="10px"
+              bg={currentPage === page ? "blue.500" : "gray.300"}
+              m={1}
+            />
+          ))}
+        </Flex>
       </ModalContent>
     </Modal>
   )
