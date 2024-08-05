84ed6706c319ad8e7084d8cb2d00791fbb207fc5
Florian Kotthoff <74312290+FlorianK13@users.noreply.github.com>
2024-08-05 15:00:32 +0200

    Extend Welcome Messages

--- src/components/Template/WelcomeMessage.js
+++ src/components/Template/WelcomeMessage.js
@@ -3,6 +3,7 @@ import {
   Button,
   Circle,
   Flex,
+  Image,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -15,13 +16,36 @@ import {
 import React, { useState } from "react"
 import { useTranslation } from "react-i18next"
 
+function WelcomeMessageBoxElement({ image, text }) {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      {image && (
+        <Image
+          src={image.src}
+          alt={image.alt}
+          style={{ maxHeight: "200px", width: "auto", margin: "20px" }}
+        />
+      )}
+
+      {text}
+    </Box>
+  )
+}
+
 function WelcomeMessage() {
   const { isOpen, onOpen, onClose } = useDisclosure({ defaultIsOpen: true })
   const [currentPage, setCurrentPage] = useState(1)
   const { t, i18n } = useTranslation()
 
+  const numPages = 4
+
   const nextPage = () => {
-    if (currentPage < 3) setCurrentPage(currentPage + 1)
+    if (currentPage < numPages) setCurrentPage(currentPage + 1)
   }
 
   const prevPage = () => {
@@ -29,15 +53,42 @@ function WelcomeMessage() {
   }
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={onClose} size="xl">
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>{t("WelcomeMessage.title")}</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          {currentPage === 1 && <Box>{t("WelcomeMessage.firstPage")}</Box>}
-          {currentPage === 2 && <Box>Second Page Content</Box>}
-          {currentPage === 3 && <Box>Third Page Content</Box>}
+          {currentPage === 1 && (
+            <WelcomeMessageBoxElement
+              image={{
+                src: "images/WelcomeMessage1.png",
+                alt: "Screenshot from the Search Input, where some address is written down.",
+              }}
+              text={t("WelcomeMessage.firstPage")}
+            />
+          )}
+          {currentPage === 2 && (
+            <WelcomeMessageBoxElement
+              image={{
+                src: "images/WelcomeMessage2.png",
+                alt: "Screenshot from a possible Simulation Result, where the solar potential of a 3D building is shown.",
+              }}
+              text={t("WelcomeMessage.secondPage")}
+            />
+          )}
+          {currentPage === 3 && (
+            <WelcomeMessageBoxElement
+              image={{
+                src: "images/WelcomeMessage3.png",
+                alt: "Screenshot from a possible Simulation Result, where a PV system was created and the annual result was calculated.",
+              }}
+              text={t("WelcomeMessage.thirdPage")}
+            />
+          )}
+          {currentPage === 4 && (
+            <WelcomeMessageBoxElement text={t("WelcomeMessage.fourthPage")} />
+          )}
         </ModalBody>
 
         <ModalFooter>
@@ -53,16 +104,13 @@ function WelcomeMessage() {
             colorScheme="blue"
             mr={3}
             onClick={nextPage}
-            isDisabled={currentPage === 3}
+            isDisabled={currentPage === numPages}
           >
             {t("next")}
           </Button>
-          <Button variant="ghost" onClick={onClose}>
-            Close
-          </Button>
         </ModalFooter>
         <Flex justifyContent="center" mb={4}>
-          {[1, 2, 3].map((page) => (
+          {Array.from({ length: numPages }, (_, i) => i + 1).map((page) => (
             <Circle
               key={page}
               size="10px"
