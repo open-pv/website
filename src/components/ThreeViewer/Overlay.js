abc8f2d2ceb4700e7fe0c5b9cd20742ada5dda90
Florian Kotthoff <74312290+FlorianK13@users.noreply.github.com>
2024-08-05 17:05:10 +0200

    Refactor Overlay Results

--- src/components/ThreeViewer/Overlay.js
+++ src/components/ThreeViewer/Overlay.js
@@ -1,4 +1,9 @@
 import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
   Box,
   Button,
   Drawer,
@@ -7,14 +12,18 @@ import {
   DrawerContent,
   DrawerHeader,
   DrawerOverlay,
+  FormLabel,
   Stack,
+  Switch,
   useDisclosure,
 } from "@chakra-ui/react"
 import { simulationForNewBuilding } from "../../simulation/main"
 
+import HoverHelp from "../Template/HoverHelp"
+import SliderWithLabel from "../Template/SliderWithLabel"
+
 import React from "react"
 import OverlayDrawPV from "./OverlayDrawPV"
-import OverlayResults from "./OverlayResults"
 
 function Overlay({
   frontendState,
@@ -36,16 +45,13 @@ function Overlay({
   const btnRef = React.useRef()
 
   return (
-    <Box position="relative" width="100%" height="100%">
+    <OverlayWrapper>
       {frontendState == "Results" && (
         <Button
           ref={btnRef}
           colorScheme="teal"
           onClick={onOpen}
           variant={"link"}
-          position="absolute"
-          top="10px"
-          left="10px"
           zIndex={100}
         >
           Optionen
@@ -59,6 +65,8 @@ function Overlay({
       )}
       {selectedMesh.length > 0 && (
         <Button
+          colorScheme="teal"
+          variant={"link"}
           className="button-high-prio"
           onClick={async () =>
             await simulationForNewBuilding({
@@ -76,33 +84,134 @@ function Overlay({
           Gebäude simulieren
         </Button>
       )}
-      <Stack spacing="24px">
-        <Drawer
-          isOpen={isOpen}
-          placement="left"
-          onClose={onClose}
-          finalFocusRef={btnRef}
-          size={"xs"}
-        >
-          <DrawerOverlay />
-          <DrawerContent height={"100%"}>
-            <DrawerCloseButton />
-            <DrawerHeader>Optionen</DrawerHeader>
+      <CustomDrawer
+        isOpen={isOpen}
+        onClose={onClose}
+        frontendState={frontendState}
+        setFrontendState={setFrontendState}
+        showTerrain={showTerrain}
+        setShowTerrain={setShowTerrain}
+      />
+    </OverlayWrapper>
+  )
+}
 
-            <DrawerBody>
-              {frontendState == "Results" && (
-                <OverlayResults
-                  setFrontendState={setFrontendState}
-                  showTerrain={showTerrain}
-                  setShowTerrain={setShowTerrain}
-                  onCloseDrawer={onClose}
+const OverlayWrapper = ({ children }) => {
+  return (
+    <>
+      <Box
+        display="flex"
+        flexDirection="column" // First direction Column and second Box flexDirection
+        // row pushes buttons to the upper left corner
+        justifyContent="space-between"
+        pointerEvents="none"
+        zIndex={100}
+        minWidth={0}
+        minHeight={0}
+        overflow="hidden"
+        sx={{
+          "> *": {
+            pointerEvents: "auto",
+          },
+        }}
+      >
+        <Box
+          display="flex"
+          flexDirection="row"
+          gap="20px"
+          padding="10px"
+          width="fit-content"
+          maxWidth="100%"
+          flexWrap="wrap"
+          minWidth={0}
+          minHeight={0}
+          overflow="hidden"
+        >
+          {children}
+        </Box>
+      </Box>
+    </>
+  )
+}
+
+const CustomDrawer = ({
+  isOpen,
+  onClose,
+  frontendState,
+  setFrontendState,
+  showTerrain,
+  setShowTerrain,
+}) => {
+  return (
+    <Stack spacing="24px">
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose} size={"xs"}>
+        <DrawerOverlay />
+        <DrawerContent height={"100%"}>
+          <DrawerCloseButton />
+          <DrawerHeader>Optionen</DrawerHeader>
+
+          <DrawerBody>
+            {frontendState == "Results" && (
+              <>
+                <Button
+                  variant={"link"}
+                  _hover={{ color: "blue.500" }}
+                  onClick={() => {
+                    setFrontendState("DrawPV")
+                    onCloseDrawer()
+                  }}
+                >
+                  PV Anlage einzeichnen
+                </Button>
+                <HoverHelp
+                  label={
+                    "PV-Anlage in der Karte einzeichnen und Jahresbetrag berechnen."
+                  }
                 />
-              )}
-            </DrawerBody>
-          </DrawerContent>
-        </Drawer>
-      </Stack>
-    </Box>
+
+                <Button variant={"link"} _hover={{ color: "blue.500" }}>
+                  Baum erstellen
+                </Button>
+                <HoverHelp
+                  label={
+                    "Legen sie einen Baum an, um diesen in der nächsten Simulation zu berücksichtigen."
+                  }
+                />
+
+                <FormLabel>
+                  Karte anzeigen
+                  <Switch
+                    isChecked={showTerrain}
+                    onChange={() => setShowTerrain((prev) => !prev)}
+                    colorScheme="teal"
+                    margin={"5px"}
+                  />
+                </FormLabel>
+                <Accordion defaultIndex={[]}>
+                  <AccordionItem>
+                    <h2>
+                      <AccordionButton>
+                        <Box as="span" flex="1" textAlign="left">
+                          Weitere Optionen
+                        </Box>
+                        <AccordionIcon />
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                      <SliderWithLabel
+                        sliderProps={{ min: 1, max: 200 }}
+                        label={"Anzahl Simulationen"}
+                        hoverHelpLabel={"Hi"}
+                      />
+                    </AccordionPanel>
+                  </AccordionItem>
+                </Accordion>
+              </>
+            )}
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </Stack>
   )
 }
 
