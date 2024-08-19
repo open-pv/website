99e0df75f5ca4cb91614c07a4c93ecba6c99db99
Florian Kotthoff <74312290+FlorianK13@users.noreply.github.com>
2024-08-19 15:17:38 +0200

    Add images to about page #203

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,8 +4,9 @@ import {
   CardBody,
   CardHeader,
   Heading,
+  Image,
+  SimpleGrid,
   Stack,
-  StackDivider,
   Text,
 } from "@chakra-ui/react"
 import React from "react"
@@ -24,6 +25,13 @@ const About = () => {
         <CardBody>
           <Stack spacing="4">
             <TextBox heading={t("about.h1")} content={t("about.p1")} />
+            <ImageRow
+              images={[
+                "images/WelcomeMessage1.png",
+                "images/WelcomeMessage2.png",
+                "images/WelcomeMessage3.png",
+              ]}
+            />
             <TextBox heading={t("about.h2")} content={t("about.p2")} />
             <TextBox heading={t("about.h3")} content={t("about.p3")} />
             <TextBox heading={t("about.h4")} content={t("about.p4")} />
@@ -44,5 +52,23 @@ function TextBox({ content, heading }) {
       </Heading>
       <Text>{content}</Text>
     </Box>
+  )
+}
+
+const ImageRow = ({ images }) => {
+  return (
+    <SimpleGrid columns={images.length} spacing={4}>
+      {images.map((src, index) => (
+        <Box key={index} padding={2}>
+          <Image
+            src={src}
+            objectFit="cover"
+            width="100%"
+            height="150px"
+            borderRadius="md"
+          />
+        </Box>
+      ))}
+    </SimpleGrid>
   )
 }
