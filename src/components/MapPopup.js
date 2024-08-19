79612ed8a1246a2a7aedb981906874d8247feb2a
Konrad Heidler <k.heidler@tum.de>
2024-08-19 10:52:17 +0200

    Make Popup close-able but reappear when latlon changes

--- src/components/MapPopup.js
+++ src/components/MapPopup.js
@@ -1,5 +1,5 @@
 import { Button } from "@chakra-ui/react"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useTranslation } from "react-i18next"
 import { Popup } from "react-map-gl/maplibre"
 import { useNavigate } from "react-router-dom"
@@ -12,18 +12,27 @@ export default function MapPopup({lat, lon, display_name}) {
     navigate(`/simulation/${lon}/${lat}`)
   };
 
-  return (
-  <Popup
-    latitude={lat}
-    longitude={lon}
-    closeButton={false}
-    closeOnClick={false}
-  >
-      { display_name }
-      <Button onClick={action}>
-        { t('startSimulation') }
-      </Button>
-  </Popup>
-  )
+  const [visible, setVisible] = useState(true);
+  useEffect(() => {
+    console.log('effect changed');
+    setVisible(true);
+  }, [lat, lon])
+
+  return <>
+    {visible && 
+      <Popup
+        latitude={lat}
+        longitude={lon}
+        // closeButton={false}
+        closeOnClick={false}
+        onClose={() => setVisible(false)}
+      >
+          { display_name }
+          <Button onClick={action}>
+            { t('startSimulation') }
+          </Button>
+      </Popup>
+    }
+  </>
 }
 
