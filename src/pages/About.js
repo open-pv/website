06cb29c5b58e66d1dd9282331811610369eac772
Florian Kotthoff <74312290+FlorianK13@users.noreply.github.com>
2024-08-24 12:27:08 +0200

    Add Footer to about page #203

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,54 +14,58 @@ import {
 } from "@chakra-ui/react"
 import React from "react"
 import { useTranslation } from "react-i18next"
+import Footer from "../components/ThreeViewer/Footer"
 
 import Main from "../Main"
 
 const About = () => {
   const { t } = useTranslation()
   return (
-    <Main title={t("about.title")} description={t("about.description")}>
-      <Card height="100%" overflow="auto">
-        <CardHeader>
-          <Heading>{t("about.description")}</Heading>
-        </CardHeader>
-        <CardBody>
-          <VStack spacing="6" align="start">
-            <TextBox heading={t("about.h1")} content={t("about.p1")}>
-              <UnorderedList>
-                <ListItem>{t("about.advantages.1")}</ListItem>
-                <ListItem>{t("about.advantages.2")}</ListItem>
-                <ListItem>{t("about.advantages.3")}</ListItem>
-              </UnorderedList>
-            </TextBox>
-            <ImageRow
-              images={[
-                "images/about/about1.png",
-                "images/WelcomeMessage2.png",
-                "images/WelcomeMessage3.png",
-              ]}
-            />
-            <TextBox heading={t("about.h2")} content={t("about.p2")} />
-            <TextBox heading={t("about.h3")} content={t("about.p3")}>
-              <Link
-                href="https://github.com/orgs/open-pv/people"
-                isExternal
-                color="teal"
-              >
-                {t("about.l3")}
-              </Link>
-            </TextBox>
+    <>
+      <Main title={t("about.title")} description={t("about.description")}>
+        <Card height="100%" overflow="auto">
+          <CardHeader>
+            <Heading>{t("about.description")}</Heading>
+          </CardHeader>
+          <CardBody>
+            <VStack spacing="6" align="start">
+              <TextBox heading={t("about.h1")} content={t("about.p1")}>
+                <UnorderedList>
+                  <ListItem>{t("about.advantages.1")}</ListItem>
+                  <ListItem>{t("about.advantages.2")}</ListItem>
+                  <ListItem>{t("about.advantages.3")}</ListItem>
+                </UnorderedList>
+              </TextBox>
+              <ImageRow
+                images={[
+                  "images/about/about1.png",
+                  "images/WelcomeMessage2.png",
+                  "images/WelcomeMessage3.png",
+                ]}
+              />
+              <TextBox heading={t("about.h2")} content={t("about.p2")} />
+              <TextBox heading={t("about.h3")} content={t("about.p3")}>
+                <Link
+                  href="https://github.com/orgs/open-pv/people"
+                  isExternal
+                  color="teal"
+                >
+                  {t("about.l3")}
+                </Link>
+              </TextBox>
 
-            <TextBox heading={t("about.h4")} content={t("about.p4")} />
-            <ImageRow
-              images={["images/about/ptf.png", "images/about/bmbf.jpg"]}
-              links={["https://prototypefund.de/", "https://www.bmbf.de"]}
-              objectFit="contain"
-            />
-          </VStack>
-        </CardBody>
-      </Card>
-    </Main>
+              <TextBox heading={t("about.h4")} content={t("about.p4")} />
+              <ImageRow
+                images={["images/about/ptf.png", "images/about/bmbf.jpg"]}
+                links={["https://prototypefund.de/", "https://www.bmbf.de"]}
+                objectFit="contain"
+              />
+            </VStack>
+          </CardBody>
+        </Card>
+      </Main>
+      <Footer />
+    </>
   )
 }
 
