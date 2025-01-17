605f3a6ef5f645fd4f57eab3bc280b87113f7d26
Florian Kotthoff <74312290+FlorianK13@users.noreply.github.com>
2025-01-17 12:16:19 +0100

    Update Loading Bar to v3 #363

---
+++ src/components/ui/progress.jsx
@@ -0,0 +1,26 @@
+import { Progress as ChakraProgress } from '@chakra-ui/react'
+import { InfoTip } from './toggle-tip'
+import * as React from 'react'
+
+export const ProgressBar = React.forwardRef(function ProgressBar(props, ref) {
+  return (
+    <ChakraProgress.Track {...props} ref={ref}>
+      <ChakraProgress.Range />
+    </ChakraProgress.Track>
+  )
+})
+
+export const ProgressLabel = React.forwardRef(
+  function ProgressLabel(props, ref) {
+    const { children, info, ...rest } = props
+    return (
+      <ChakraProgress.Label {...rest} ref={ref}>
+        {children}
+        {info && <InfoTip>{info}</InfoTip>}
+      </ChakraProgress.Label>
+    )
+  },
+)
+
+export const ProgressRoot = ChakraProgress.Root
+export const ProgressValueText = ChakraProgress.ValueText
