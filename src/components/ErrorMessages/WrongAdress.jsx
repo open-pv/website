8bb55351505b4a1230c5e58b5160988ffa59cf4c
Florian Kotthoff <74312290+FlorianK13@users.noreply.github.com>
2025-01-17 12:13:07 +0100

    Update WrongAdress to chakra v3 #363

--- src/components/ErrorMessages/WrongAdress.jsx
+++ src/components/ErrorMessages/WrongAdress.jsx
@@ -1,16 +1,16 @@
-import { Card, CardBody, CardHeader, Heading } from '@chakra-ui/react'
+import { Card } from '@chakra-ui/react'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
 function WrongAdress() {
   const { t } = useTranslation()
   return (
-    <Card>
-      <CardHeader>
-        <Heading>{t('errorMessage.header')}</Heading>
-      </CardHeader>
-      <CardBody>{t('errorMessage.wrongAdress')}</CardBody>
-    </Card>
+    <Card.Root>
+      <Card.Body gap='2'>
+        <Card.Title mt='2'>{t('errorMessage.header')}</Card.Title>
+        <Card.Description>{t('errorMessage.wrongAdress')}</Card.Description>
+      </Card.Body>
+    </Card.Root>
   )
 }
 
