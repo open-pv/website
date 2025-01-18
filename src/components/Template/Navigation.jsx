9c0ef611f1162cf470c2d4e95a8d5574dd8a1430
Florian Kotthoff <74312290+FlorianK13@users.noreply.github.com>
2025-01-18 13:11:53 +0100

    Fix navigation in chakrav3 #363

--- src/components/Template/Navigation.jsx
+++ src/components/Template/Navigation.jsx
@@ -1,25 +1,32 @@
 import { Link, Tabs } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const Navigation = () => {
   const { t } = useTranslation()
-  const navigate = useNavigate()
+  const location = useLocation()
+  const isActive = (path) => location.pathname === path
 
   return (
-    <Tabs.Root navigate={({ value, node }) => navigate(`/${value}`)} size='lg'>
-      <Tabs.List>
+    <Tabs.Root value={isActive('/about') ? '/about' : '/'}>
+      <Tabs.List bg='bg.muted' rounded='l3'>
         <Tabs.Trigger value='/'>
-          <Link unstyled href='/'>
+          <Link textStyle='xl' href='/'>
             OpenPV
           </Link>
         </Tabs.Trigger>
-        <Tabs.Trigger value='about'>
+        <Tabs.Trigger value='/about'>
           <Link unstyled href='/about'>
             {t('about.title')}
           </Link>
         </Tabs.Trigger>
+        <Tabs.Trigger value='blog'>
+          <Link unstyled href='https://blog.openpv.de'>
+            {t('navigation.products')}
+          </Link>
+        </Tabs.Trigger>
+        <Tabs.Indicator rounded='l2' />
       </Tabs.List>
     </Tabs.Root>
   )
