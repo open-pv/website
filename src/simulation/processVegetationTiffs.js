666455599563d56fbaeee96ad69bb64828d314e3
Konrad Heidler <k.heidler@tum.de>
2025-01-18 23:29:28 +0100

    Triangulate vegetation

--- src/simulation/processVegetationTiffs.js
+++ src/simulation/processVegetationTiffs.js
@@ -50,60 +50,78 @@ export function processVegetationData(
   const centerX = simulationCenter.x || 0
   const centerY = simulationCenter.y || 0
 
-  for (let y = 0; y < vegetationRaster.height; y++) {
-    for (let x = 0; x < vegetationRaster.width; x++) {
-      const height = vegetationRaster.data[y * vegetationRaster.width + x]
+  const x0 = vegetationRaster.bbox[0] - cx
+  const dx =
+    (vegetationRaster.bbox[2] - vegetationRaster.bbox[0]) /
+    vegetationRaster.width
+  const y0 = vegetationRaster.bbox[3] - cy
+  const dy =
+    (vegetationRaster.bbox[1] - vegetationRaster.bbox[3]) /
+    vegetationRaster.height
 
-      if (height > 0) {
-        // Convert from raster coordinates to simulation coordinates
-        const webMercatorX =
-          vegetationRaster.bbox[0] +
-          (vegetationRaster.bbox[2] - vegetationRaster.bbox[0]) *
-            (x / vegetationRaster.width)
-        const webMercatorY =
-          vegetationRaster.bbox[3] +
-          (vegetationRaster.bbox[1] - vegetationRaster.bbox[3]) *
-            (y / vegetationRaster.height)
-        const simX = (webMercatorX - cx) * mercator2meters()
-        const simY = (webMercatorY - cy) * mercator2meters()
+  let surroundingTriangles = []
+  let backgroundTriangles = []
 
-        if ((x % 100) + (y % 100) == 0) {
-          console.log(simX, simY)
+  for (let y = 0; y < vegetationRaster.height - 1; y++) {
+    for (let x = 0; x < vegetationRaster.width - 1; x++) {
+      const w = vegetationRaster.width
+      const h00 = vegetationRaster.data[y * w + x]
+      const h10 = vegetationRaster.data[y * w + x + 1]
+      const h01 = vegetationRaster.data[(y + 1) * w + x]
+      const h11 = vegetationRaster.data[(y + 1) * w + x + 1]
+
+      const sx0 = (x0 + dx * x) * mercator2meters()
+      const sx1 = (x0 + dx * (x + 1)) * mercator2meters()
+      const sy0 = (y0 + dy * y) * mercator2meters()
+      const sy1 = (y0 + dy * (y + 1)) * mercator2meters()
+
+      // Triangle candidates
+      const tris = [
+        [sx0, sy0, h00, sx1, sy0, h10, sx0, sy1, h01],
+        [sx0, sy1, h01, sx1, sy0, h10, sx1, sy1, h11],
+      ]
+      for (let tri of tris) {
+        // If all heights are 0, don't render triangle
+        if (tri[2] == 0 && tri[5] == 0 && tri[8] == 0) {
+          continue
         }
+        const mx = (tri[0] + tri[3] + tri[6]) / 3
+        const my = (tri[1] + tri[4] + tri[7]) / 3
 
-        // Calculate distance squared from simulation center
-        const distanceSquared =
-          (simX - centerX) * (simX - centerX) +
-          (simY - centerY) * (simY - centerY)
-
-        if (distanceSquared <= viewingCutoffSquared) {
-          const vegGeometry = createVegetationGeometry(simX, simY, height)
-
-          if (distanceSquared <= simulationCutoffSquared) {
-            geometries.surrounding.push(vegGeometry)
+        const d2 =
+          (centerX - mx) * (centerX - mx) + (centerY - my) * (centerY - my)
+        if (d2 <= viewingCutoffSquared) {
+          if (d2 <= simulationCutoffSquared) {
+            surroundingTriangles = surroundingTriangles.concat(tri)
           } else {
-            geometries.background.push(vegGeometry)
+            backgroundTriangles = backgroundTriangles.concat(tri)
           }
         }
       }
     }
   }
 
-  console.log('Vegetation processing complete.')
-  console.log('Surrounding geometries:', geometries.surrounding.length)
-  console.log('Background geometries:', geometries.background.length)
+  if (surroundingTriangles.length > 0) {
+    const surroundingGeom = new THREE.BufferGeometry()
+    const surroundingPos = new THREE.BufferAttribute(
+      new Float32Array(surroundingTriangles),
+      3,
+    )
+    surroundingGeom.setAttribute('position', surroundingPos)
+    surroundingGeom.computeVertexNormals()
+    geometries.surrounding.push(surroundingGeom)
+  }
+
+  if (backgroundTriangles.length > 0) {
+    const backgroundGeom = new THREE.BufferGeometry()
+    const backgroundPos = new THREE.BufferAttribute(
+      new Float32Array(backgroundTriangles),
+      3,
+    )
+    backgroundGeom.setAttribute('position', backgroundPos)
+    backgroundGeom.computeVertexNormals()
+    geometries.background.push(backgroundGeom)
+  }
 
   return geometries
-}
-
-function createVegetationGeometry(x, y, vegHeight) {
-  const width = 1
-  const depth = 1
-
-  const geometry = new THREE.BoxGeometry(width, depth, 20)
-    .toNonIndexed()
-    .deleteAttribute('uv')
-  geometry.translate(x, y, vegHeight - 10)
-
-  return geometry
 }
