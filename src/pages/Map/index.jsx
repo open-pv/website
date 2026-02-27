2dd8d6f975dfdfc9bcd17e23b24638e375c838de
FlorianK13 <florian.kotthoff@posteo.de>
2026-02-27 10:16:10 +0100

    Fix h1 tags #568

--- src/pages/Map/index.jsx
+++ src/pages/Map/index.jsx
@@ -95,6 +95,18 @@ function Index() {
 
   return (
     <App description={t('mainDescription')}>
+      <h1
+        style={{
+          position: 'absolute',
+          width: '1px',
+          height: '1px',
+          overflow: 'hidden',
+          clip: 'rect(0,0,0,0)',
+          whiteSpace: 'nowrap',
+        }}
+      >
+        {t('title')}
+      </h1>
       <header>
         <div className='title'>
           <SearchField callback={searchCallback} />
