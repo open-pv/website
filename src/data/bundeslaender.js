c4664840af7348fe6788dd3171b2718db7db4b90
FlorianK13 <florian.kotthoff@posteo.de>
2026-02-27 10:56:43 +0100

    Add url parameter for special locations #568

---
+++ src/data/bundeslaender.js
@@ -0,0 +1,36 @@
+export const BUNDESLAENDER = {
+  'Baden-Wuerttemberg': {
+    name: 'Baden-Württemberg',
+    bounds: [7.5, 47.6, 10.5, 49.8],
+  },
+  Bayern: { name: 'Bayern', bounds: [10.0, 47.3, 13.8, 50.6] },
+  Berlin: { name: 'Berlin', bounds: [13.1, 52.3, 13.8, 52.7] },
+  Brandenburg: { name: 'Brandenburg', bounds: [11.3, 51.4, 14.8, 53.6] },
+  Bremen: { name: 'Bremen', bounds: [8.5, 53.0, 8.9, 53.2] },
+  Hamburg: { name: 'Hamburg', bounds: [9.7, 53.4, 10.3, 53.7] },
+  Hessen: { name: 'Hessen', bounds: [7.8, 49.4, 10.2, 51.7] },
+  'Mecklenburg-Vorpommern': {
+    name: 'Mecklenburg-Vorpommern',
+    bounds: [10.6, 53.1, 14.4, 54.7],
+  },
+  Niedersachsen: { name: 'Niedersachsen', bounds: [6.7, 51.3, 11.6, 53.9] },
+  'Nordrhein-Westfalen': {
+    name: 'Nordrhein-Westfalen',
+    bounds: [6.0, 50.3, 9.5, 52.5],
+  },
+  'Rheinland-Pfalz': {
+    name: 'Rheinland-Pfalz',
+    bounds: [6.1, 49.1, 8.5, 51.0],
+  },
+  Saarland: { name: 'Saarland', bounds: [6.3, 49.1, 7.4, 49.6] },
+  Sachsen: { name: 'Sachsen', bounds: [12.0, 50.2, 15.0, 51.7] },
+  'Sachsen-Anhalt': {
+    name: 'Sachsen-Anhalt',
+    bounds: [10.6, 51.0, 13.2, 53.1],
+  },
+  'Schleswig-Holstein': {
+    name: 'Schleswig-Holstein',
+    bounds: [8.0, 53.4, 11.0, 55.1],
+  },
+  Thueringen: { name: 'Thüringen', bounds: [9.9, 50.2, 12.7, 51.6] },
+}
